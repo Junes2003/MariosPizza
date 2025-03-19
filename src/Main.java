cd5e305ecdb4561873139a989158bcab9962a2cc
Aden0020 <aden0020@hotmail.com>
2025-03-19 13:24:48 +0100

    Added change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mo er grim");
+        System.out.println("mo er gay");
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,6 @@
 public class Product {
+    private String name;
+    private double price;
+    public String ingredients;
 
 }
