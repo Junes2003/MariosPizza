5b9cc7b4c05178ef623d86936231769eaf27c2ce
Junes2003 <juel0001@stud.kea.dk>
2025-03-20 13:20:11 +0100

    Added change

--- src/Menu.java
+++ src/Menu.java
@@ -5,36 +5,37 @@ public class Menu {
 
     public Menu() {
         this.menu = new ArrayList<>();
-        menu.add("\"Nr. 1 Vesuvio\", \"Tomatsauce, ost, skinke\", 72));");
-        menu.add("\"Nr. 2 Amerikaner\", \"Tomatsauce, ost, pepperoni\", 68));");
-        menu.add("\"Nr. 3 Cacciatore\", \"Tomatsauce, ost, skinke, champignon\", 72))");
-        menu.add("\"Nr. 4 Carbonara\", \"Tomatsauce, ost, æg, bacon\", 80))");
-        menu.add("\"Nr. 5 Dennis\", \"Tomatsauce, ost, skinke, pepperoni, bacon\", 83))");
-        menu.add("\"Nr. 6 Bertil\", \"Tomatsauce, ost, skinke, løg\", 72))");
-        menu.add("\"Nr. 7 Silvia\", \"Tomatsauce, ost, artiskok, oliven\", 76))");
-        menu.add("\"Nr. 8 Victoria\", \"Tomatsauce, ost, skinke, champignon, peberfrugt\", 76))");
-        menu.add("\"Nr. 9 Toronfo\", \"Tomatsauce, ost, kødsauce, løg, hvidløg\", 76))");
-        menu.add("\"Nr. 10 Capricciosa\", \"Tomatsauce, ost, skinke, champignon, oliven\", 76))");
-        menu.add("\"Nr. 11 Hawaii\", \"Tomatsauce, ost, skinke, ananas\", 76))");
-        menu.add("\"Nr. 12 Le Blissola\", \"Tomatsauce, ost, skinke, rejer\", 76))");
-        menu.add("\"Nr. 13 Venezia\", \"Tomatsauce, ost, skinke, rejer\", 76))");
-        menu.add("\"Nr. 14 Mafia\", \"Tomatsauce, ost, pepperoni, hvidløg\", 76))");
-        menu.add("\"Nr. 15 Diavola\", \"Tomatsauce, ost, stærk salami, chili\", 82));");
-        menu.add("\"Nr. 16 Quattro Formaggi\", \"Tomatsauce, mozzarella, gorgonzola, parmesan, ricotta\", 85));");
-        menu.add("\"Nr. 17 Parma\", \"Tomatsauce, ost, parmaskinke, rucola, parmesan\", 90));");
-        menu.add("\"Nr. 18 Margherita\", \"Tomatsauce, ost, frisk basilikum\", 65));");
-        menu.add("\"Nr. 19 Quattro Stagioni\", \"Tomatsauce, ost, skinke, champignon, artiskok, oliven\", 80));");
-        menu.add("\"Nr. 20 Rustica\", \"Tomatsauce, ost, kartoffel, rosmarin, bacon\", 82));");
-        menu.add("\"Nr. 21 Frutti di Mare\", \"Tomatsauce, ost, blandede skaldyr, hvidløg\", 88));");
-        menu.add("\"Nr. 22 Calzone\", \"Tomatsauce, ost, skinke, champignon (indbagt)\", 78));");
-        menu.add("\"Nr. 23 BBQ Chicken\", \"BBQ-sauce, ost, kylling, rødløg\", 84));");
-        menu.add("\"Nr. 24 Toscana\", \"Tomatsauce, ost, salsiccia (italiensk pølse), peberfrugt\", 86));");
-        menu.add("\"Nr. 25 Vegetariana\", \"Tomatsauce, ost, champignon, peberfrugt, oliven, løg\", 78));");
-        menu.add("\"Nr. 26 Bianca\", \"Fløde, mozzarella, kartoffel, rosmarin, trøffelolie\", 90));");
-        menu.add("\"Nr. 27 Inferno\", \"Tomatsauce, ost, stærk pepperoni, jalapeños, chiliolie\", 85));");
-        menu.add("\"Nr. 28 Napoli\", \"Tomatsauce, ost, ansjoser, kapers, oliven\", 79));");
-        menu.add("\"Nr. 29 Gorgonzola\", \"Tomatsauce, ost, gorgonzola, pærer, valnødder\", 89));");
-        menu.add("\"Nr. 30 Mexicana\", \"Tomatsauce, ost, hakket oksekød, jalapeños, creme fraiche\", 84));");
+        menu.add("\"No. 1 Vesuvio\", \"Tomato sauce, cheese, ham\", 72));");
+        menu.add("\"No. 2 American\", \"Tomato sauce, cheese, pepperoni\", 68));");
+        menu.add("\"No. 3 Cacciatore\", \"Tomato sauce, cheese, ham, mushrooms\", 72))");
+        menu.add("\"No. 4 Carbonara\", \"Tomato sauce, cheese, egg, bacon\", 80))");
+        menu.add("\"No. 5 Dennis\", \"Tomato sauce, cheese, ham, pepperoni, bacon\", 83))");
+        menu.add("\"No. 6 Bertil\", \"Tomato sauce, cheese, ham, onion\", 72))");
+        menu.add("\"No. 7 Silvia\", \"Tomato sauce, cheese, artichoke, olives\", 76))");
+        menu.add("\"No. 8 Victoria\", \"Tomato sauce, cheese, ham, mushrooms, bell pepper\", 76))");
+        menu.add("\"No. 9 Toronfo\", \"Tomato sauce, cheese, meat sauce, onion, garlic\", 76))");
+        menu.add("\"No. 10 Capricciosa\", \"Tomato sauce, cheese, ham, mushrooms, olives\", 76))");
+        menu.add("\"No. 11 Hawaii\", \"Tomato sauce, cheese, ham, pineapple\", 76))");
+        menu.add("\"No. 12 Le Blissola\", \"Tomato sauce, cheese, ham, shrimp\", 76))");
+        menu.add("\"No. 13 Venezia\", \"Tomato sauce, cheese, ham, shrimp\", 76))");
+        menu.add("\"No. 14 Mafia\", \"Tomato sauce, cheese, pepperoni, garlic\", 76))");
+        menu.add("\"No. 15 Diavola\", \"Tomato sauce, cheese, spicy salami, chili\", 82));");
+        menu.add("\"No. 16 Quattro Formaggi\", \"Tomato sauce, mozzarella, gorgonzola, parmesan, ricotta\", 85));");
+        menu.add("\"No. 17 Parma\", \"Tomato sauce, cheese, Parma ham, arugula, parmesan\", 90));");
+        menu.add("\"No. 18 Margherita\", \"Tomato sauce, cheese, fresh basil\", 65));");
+        menu.add("\"No. 19 Quattro Stagioni\", \"Tomato sauce, cheese, ham, mushrooms, artichoke, olives\", 80));");
+        menu.add("\"No. 20 Rustica\", \"Tomato sauce, cheese, potato, rosemary, bacon\", 82));");
+        menu.add("\"No. 21 Frutti di Mare\", \"Tomato sauce, cheese, mixed seafood, garlic\", 88));");
+        menu.add("\"No. 22 Calzone\", \"Tomato sauce, cheese, ham, mushrooms (folded pizza)\", 78));");
+        menu.add("\"No. 23 BBQ Chicken\", \"BBQ sauce, cheese, chicken, red onion\", 84));");
+        menu.add("\"No. 24 Toscana\", \"Tomato sauce, cheese, Italian sausage, bell pepper\", 86));");
+        menu.add("\"No. 25 Vegetariana\", \"Tomato sauce, cheese, mushrooms, bell pepper, olives, onion\", 78));");
+        menu.add("\"No. 26 Bianca\", \"Cream, mozzarella, potato, rosemary, truffle oil\", 90));");
+        menu.add("\"No. 27 Inferno\", \"Tomato sauce, cheese, spicy pepperoni, jalapeños, chili oil\", 85));");
+        menu.add("\"No. 28 Napoli\", \"Tomato sauce, cheese, anchovies, capers, olives\", 79));");
+        menu.add("\"No. 29 Gorgonzola\", \"Tomato sauce, cheese, gorgonzola, pears, walnuts\", 89));");
+        menu.add("\"No. 30 Mexicana\", \"Tomato sauce, cheese, minced beef, jalapeños, sour cream\", 84));");
+
 
     }
 }
