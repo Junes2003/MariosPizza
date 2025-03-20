d2388e2c7599cb059b78c3ad8e2e9a4f9fdf004b
aljamour <alal0002@stud.kea.dk>
2025-03-20 14:12:37 +0100

    Added change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Junes er gay");
+        Menu menu = new Menu();
+        menu.printMenu();
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -36,6 +36,11 @@ public class Menu {
         menu.add("\"No. 29 Alan\", \"Tomato sauce, cheese, gorgonzola, pears, walnuts\", 89));");
         menu.add("\"No. 30 Aden\", \"Tomato sauce, cheese, minced beef, jalapeños, sour cream\", 84));");
 
+    }
 
+    public void printMenu(){
+        for (String element : menu){
+            System.out.println(element);
+        }
     }
 }
