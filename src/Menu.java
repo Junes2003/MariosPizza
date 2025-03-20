0b257cde4fc902b7df3721f07aa278b4cdfa601c
Aden0020 <aden0020@hotmail.com>
2025-03-20 12:29:29 +0100

    added change

--- src/Menu.java
+++ src/Menu.java
@@ -4,17 +4,17 @@ public class Menu {
 
 
 //        ("Nr. 1 Vesuvio", "Tomatsauce, ost, skinke", 72));
-//        (new Pizza("Nr. 2 Amerikaner", "Tomatsauce, ost, pepperoni", 68));
-//        (new Pizza("Nr. 3 Cacciatore", "Tomatsauce, ost, skinke, champignon", 72));
-//        (new Pizza("Nr. 4 Carbonara", "Tomatsauce, ost, æg, bacon", 80));
-//        (new Pizza("Nr. 5 Dennis", "Tomatsauce, ost, skinke, pepperoni, bacon", 83));
-//        (new Pizza("Nr. 6 Bertil", "Tomatsauce, ost, skinke, løg", 72));
-//        (new Pizza("Nr. 7 Silvia", "Tomatsauce, ost, artiskok, oliven", 76));
-//        (new Pizza("Nr. 8 Victoria", "Tomatsauce, ost, skinke, champignon, peberfrugt", 76));
-//        (new Pizza("Nr. 9 Toronfo", "Tomatsauce, ost, kødsauce, løg, hvidløg", 76));
-//        (new Pizza("Nr. 10 Capricciosa", "Tomatsauce, ost, skinke, champignon, oliven", 76));
-//        (new Pizza("Nr. 11 Hawaii", "Tomatsauce, ost, skinke, ananas", 76));
-//        (new Pizza("Nr. 12 Le Blissola", "Tomatsauce, ost, skinke, rejer", 76));
-//        (new Pizza("Nr. 13 Venezia", "Tomatsauce, ost, skinke, rejer", 76));
-//        (new Pizza("Nr. 14 Mafia", "Tomatsauce, ost, pepperoni, hvidløg", 76));
+//        ("Nr. 2 Amerikaner", "Tomatsauce, ost, pepperoni", 68));
+//        ("Nr. 3 Cacciatore", "Tomatsauce, ost, skinke, champignon", 72));
+//        ("Nr. 4 Carbonara", "Tomatsauce, ost, æg, bacon", 80));
+//        ("Nr. 5 Dennis", "Tomatsauce, ost, skinke, pepperoni, bacon", 83));
+//        ("Nr. 6 Bertil", "Tomatsauce, ost, skinke, løg", 72));
+//        ("Nr. 7 Silvia", "Tomatsauce, ost, artiskok, oliven", 76));
+//        ("Nr. 8 Victoria", "Tomatsauce, ost, skinke, champignon, peberfrugt", 76));
+//        ("Nr. 9 Toronfo", "Tomatsauce, ost, kødsauce, løg, hvidløg", 76));
+//        ("Nr. 10 Capricciosa", "Tomatsauce, ost, skinke, champignon, oliven", 76));
+//        ("Nr. 11 Hawaii", "Tomatsauce, ost, skinke, ananas", 76));
+//        ("Nr. 12 Le Blissola", "Tomatsauce, ost, skinke, rejer", 76));
+//        ("Nr. 13 Venezia", "Tomatsauce, ost, skinke, rejer", 76));
+//        ("Nr. 14 Mafia", "Tomatsauce, ost, pepperoni, hvidløg", 76));
 
