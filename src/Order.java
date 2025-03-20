fde1e6b1c3b09ebe758691bb1cb3b450b6e2be63
aljamour <alal0002@stud.kea.dk>
2025-03-20 14:58:29 +0100

    Added change

--- src/Order.java
+++ src/Order.java
@@ -1,8 +1,13 @@
-public class Order extends Product {
+import java.util.Scanner;
+
+public class Order {
     private String customerOrder;
     private int orderID;
     private double totalPrice;
     private double orderTime;
+
+    public Order(){
+    }
 
     public Order(String customerOrder, int orderID, double totalPrice, double orderTime) {
         this.customerOrder = customerOrder;
@@ -12,6 +17,23 @@ public class Order extends Product {
     }
 
     public void addProduct(Product product) {
+    }
+
+    public void printLogin(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Velkommen til! Indtast kode for at starte systemet:");
+        String startSystem = scan.nextLine();
+
+        String code = "Osman";
+
+        if (startSystem.equals(code)) {
+            System.out.println("Du er nu logget ind");
+        } else if (!startSystem.equals(code)) {
+            while (true) {
+                System.out.println("Ugyldigt input! Prøv igen");
+                String retry = scan.nextLine();
+            }
+        }
 
     }
 }
