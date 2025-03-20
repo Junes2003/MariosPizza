c8e679a46e3daebfbd77338c2fc731abb8260681
Junes2003 <juel0001@stud.kea.dk>
2025-03-20 12:25:23 +0100

    Added change

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+
 public class Menu {
 
+    private ArrayList<String>menu;
+
+    public Menu() {
+        this.menu = new ArrayList<>();
+        menu.add("");
+        menu.add("");
+        menu.add("");
+        menu.add("");
+        menu.add("");
+        menu.add("");
+        menu.add("");
+        menu.add("");
+        menu.add("");
+        menu.add("");
+        menu.add("");
+        menu.add("");
+
+
+    }
 }
