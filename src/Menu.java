372901f890b7470f6a29dbdda070e629892230fa
Junes2003 <juel0001@stud.kea.dk>
2025-03-20 13:01:16 +0100

    Added change

--- src/Menu.java
+++ src/Menu.java
@@ -19,7 +19,22 @@ public class Menu {
         menu.add("\"Nr. 12 Le Blissola\", \"Tomatsauce, ost, skinke, rejer\", 76))");
         menu.add("\"Nr. 13 Venezia\", \"Tomatsauce, ost, skinke, rejer\", 76))");
         menu.add("\"Nr. 14 Mafia\", \"Tomatsauce, ost, pepperoni, hvidløg\", 76))");
-
+        menu.add("\"Nr. 15 Diavola\", \"Tomatsauce, ost, stærk salami, chili\", 82));");
+        menu.add("\"Nr. 16 Quattro Formaggi\", \"Tomatsauce, mozzarella, gorgonzola, parmesan, ricotta\", 85));");
+        menu.add("\"Nr. 17 Parma\", \"Tomatsauce, ost, parmaskinke, rucola, parmesan\", 90));");
+        menu.add("\"Nr. 18 Margherita\", \"Tomatsauce, ost, frisk basilikum\", 65));");
+        menu.add("\"Nr. 19 Quattro Stagioni\", \"Tomatsauce, ost, skinke, champignon, artiskok, oliven\", 80));");
+        menu.add("\"Nr. 20 Rustica\", \"Tomatsauce, ost, kartoffel, rosmarin, bacon\", 82));");
+        menu.add("\"Nr. 21 Frutti di Mare\", \"Tomatsauce, ost, blandede skaldyr, hvidløg\", 88));");
+        menu.add("\"Nr. 22 Calzone\", \"Tomatsauce, ost, skinke, champignon (indbagt)\", 78));");
+        menu.add("\"Nr. 23 BBQ Chicken\", \"BBQ-sauce, ost, kylling, rødløg\", 84));");
+        menu.add("\"Nr. 24 Toscana\", \"Tomatsauce, ost, salsiccia (italiensk pølse), peberfrugt\", 86));");
+        menu.add("\"Nr. 25 Vegetariana\", \"Tomatsauce, ost, champignon, peberfrugt, oliven, løg\", 78));");
+        menu.add("\"Nr. 26 Bianca\", \"Fløde, mozzarella, kartoffel, rosmarin, trøffelolie\", 90));");
+        menu.add("\"Nr. 27 Inferno\", \"Tomatsauce, ost, stærk pepperoni, jalapeños, chiliolie\", 85));");
+        menu.add("\"Nr. 28 Napoli\", \"Tomatsauce, ost, ansjoser, kapers, oliven\", 79));");
+        menu.add("\"Nr. 29 Gorgonzola\", \"Tomatsauce, ost, gorgonzola, pærer, valnødder\", 89));");
+        menu.add("\"Nr. 30 Mexicana\", \"Tomatsauce, ost, hakket oksekød, jalapeños, creme fraiche\", 84));");
 
     }
 }
