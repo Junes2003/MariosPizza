c01b7eeb00bf688374f7063f70ee1207f60547b0
aljamour <alal0002@stud.kea.dk>
2025-03-20 13:54:26 +0100

    Added change

--- src/Order.java
+++ src/Order.java
@@ -1,3 +1,17 @@
-public class Order {
+public class Order extends Product {
+    private String customerOrder;
+    private int orderID;
+    private double totalPrice;
+    private double orderTime;
 
+    public Order(String customerOrder, int orderID, double totalPrice, double orderTime) {
+        this.customerOrder = customerOrder;
+        this.orderID = orderID;
+        this.totalPrice = totalPrice;
+        this.orderTime = orderTime;
+    }
+
+    public void addProduct(Product product) {
+
+    }
 }
