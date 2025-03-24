68f1630411076ed71c03cd8a81aa4ddea8ec4dbe
Junes2003 <juel0001@stud.kea.dk>
2025-03-24 12:22:41 +0100

    Added change

--- src/Accountancy.java
+++ src/Accountancy.java
@@ -7,4 +7,16 @@ class Accountancy {
     public Accountancy() {
         completedOrders = new ArrayList<>();
     }
+    public void addCompletedOrder(Order order) {
+        completedOrders.add(order);
+    }
+
+    public double getTotalRevenue() {
+        return // noget med en sum
+    }
+
+    public void printRevenue() {
+        System.out.println("Total revenue: " + getTotalRevenue() + " DKK");
+    }
+}
 }
