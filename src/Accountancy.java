87b4a024f1657206acd2dadb54b818d7ad1d1c9b
aljamour <alal0002@stud.kea.dk>
2025-03-24 13:46:33 +0100

    Added change

--- src/Accountancy.java
+++ src/Accountancy.java
@@ -11,12 +11,12 @@ class Accountancy {
         completedOrders.add(order);
     }
 
-    public double getTotalRevenue() {
-        return // noget med en sum
-    }
+//    public double getTotalRevenue() {
+//        return // noget med en sum
+//    }
 
-    public void printRevenue() {
-        System.out.println("Total revenue: " + getTotalRevenue() + " DKK");
-    }
+//    public void printRevenue() {
+//        System.out.println("Total revenue: " + getTotalRevenue() + " DKK");
+//    }
 }
 
