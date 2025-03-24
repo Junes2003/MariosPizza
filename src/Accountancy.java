dd4b153d699d64ca931a0f1691fa4b9d53821379
Junes2003 <juel0001@stud.kea.dk>
2025-03-24 12:01:29 +0100

    Added change

--- src/Accountancy.java
+++ src/Accountancy.java
@@ -1,3 +1,10 @@
-public class Accountancy {
+import java.util.ArrayList;
+import java.util.List;
 
+class Accountancy {
+    private List<Order> completedOrders;
+
+    public Accountancy() {
+        completedOrders = new ArrayList<>();
+    }
 }
